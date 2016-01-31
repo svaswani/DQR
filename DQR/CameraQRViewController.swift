934b2f0d2c7427ca211be9df5bb106184b631856
Nick Mosher <nicholastmosher@gmail.com>
2016-01-30 22:19:40 -0500

    Moved QR generator into PlayerItem.

--- DQR/CameraQRViewController.swift
+++ DQR/CameraQRViewController.swift
@@ -91,21 +91,5 @@ class CameraQRViewController: UIViewController, AVCaptureMetadataOutputObjectsDe
             }
         }
     }
-    
-    func generateQR(text: String) -> UIImage {
-        
-        // Convert string data into a form usable for QR generation.
-        let data = text.dataUsingEncoding(NSISOLatin1StringEncoding, allowLossyConversion: false)
-        
-        // Create a filter that will parse the text into a QR image.
-        let filter = CIFilter(name: "CIQRCodeGenerator")
-        
-        //Set filter properties to "tune" QR image.
-        filter!.setValue(data, forKey: "inputMessage")
-        filter!.setValue("Q", forKey: "inputCorrectionLabel")
-        
-        // Return the generated QR image.
-        return UIImage(CIImage: (filter?.outputImage)!)
-    }
 }
 
--- DQR/PlayerItem.swift
+++ DQR/PlayerItem.swift
@@ -11,11 +11,27 @@ import UIKit
 class PlayerItem: NSObject {
     
     let name: String
-    //var qRCode:UIImage
+    var qrImage: UIImage
     
     init(name: String) {
         self.name = name
-        //self.qRCode = qRCode
+        self.qrImage = PlayerItem.generateQR(name)
     }
     
+    // Generates a QR code from a string and returns a UIImage.
+    class func generateQR(text: String) -> UIImage {
+        
+        // Convert string data into a form usable for QR generation.
+        let data = text.dataUsingEncoding(NSISOLatin1StringEncoding, allowLossyConversion: false)
+        
+        // Create a filter that will parse the text into a QR image.
+        let filter = CIFilter(name: "CIQRCodeGenerator")
+        
+        //Set filter properties to "tune" QR image.
+        filter!.setValue(data, forKey: "inputMessage")
+        filter!.setValue("Q", forKey: "inputCorrectionLabel")
+        
+        // Return the generated QR image.
+        return UIImage(CIImage: (filter?.outputImage)!)
+    }
 }
