dfad048d0a4b9ebe0413149d5f7c7f2c38539307
Wm. Blake Sullivan <wbsllvn@gmail.com>
2016-01-31 01:55:12 -0500

    Added archiving capabilities to player item

---
+++ iOS/DQR/PlayerManager.swift
@@ -0,0 +1,14 @@
+//
+//  PlayerManager.swift
+//  DQR
+//
+//  Created by Wm. Blake Sullivan on 1/31/16.
+//  Copyright © 2016 Team DQR. All rights reserved.
+//
+
+import UIKit
+
+class PlayerManager {
+    
+    var playerList = [PlayerItem]()
+}
