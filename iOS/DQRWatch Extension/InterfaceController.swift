28d33c098a9e4a10202582642e425bd4793a751f
Wm. Blake Sullivan <wbsllvn@gmail.com>
2016-01-30 23:24:27 -0500

    Added base watchOS files

---
+++ iOS/DQRWatch Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  DQRWatch Extension
+//
+//  Created by Wm. Blake Sullivan on 1/30/16.
+//  Copyright © 2016 Team DQR. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
