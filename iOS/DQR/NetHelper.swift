9a9f1a24bbe3964618535dcf4b6f387b26cd17ac
Nick Mosher <nicholastmosher@gmail.com>
2016-01-31 18:23:13 -0500

    Fixed merge conflict with NetHelper.

--- iOS/DQR/NetHelper.swift
+++ iOS/DQR/NetHelper.swift
@@ -18,7 +18,7 @@ class NetHelper : NSObject, NSStreamDelegate {
 var inputStream: NSInputStream?
 var outputStream: NSOutputStream?
     
-    override init() {
+    private override init() {
         super.init()
         
         NSStream.getStreamsToHostWithName(server, port: port, inputStream: &inputStream, outputStream: &outputStream)
@@ -53,48 +53,5 @@ var outputStream: NSOutputStream?
         print("Sending \(text) to server.")
         let buffer: [UInt8] = Array(text.utf8)
         outputStream?.write(buffer, maxLength: text.lengthOfBytesUsingEncoding(NSUTF8StringEncoding))
-    }
-    
-    func stream(aStream: NSStream, handleEvent eventCode: NSStreamEvent) {
-        
-        print("Callback started")
-        
-        // The input stream is the only stream we resistered as a delegate to, but this is a safety check.
-        if aStream == inputStream {
-            
-            print("Callback is from inputStream")
-            
-            // Perform different actions based on the event code.
-            switch eventCode {
-                
-            case NSStreamEvent.HasBytesAvailable:
-                
-                print("NSStreamEvent has bytes available")
-                
-                // Create a buffer to store the read data into.
-                var buffer = [UInt8](count: 4096, repeatedValue: 0)
-                
-                // While the input stream still has data, continue reading it.
-                while ((inputStream?.hasBytesAvailable) != nil) {
-                    
-                    // Read available bytes into the buffer and return the length of bytes read.
-                    let len = inputStream?.read(&buffer, maxLength: buffer.count)
-                    if(len > 0) {
-                        
-                        // Convert the bytes in the buffer to a string using UTF8.
-                        let output = NSString(bytes: &buffer, length: buffer.count, encoding: NSUTF8StringEncoding)
-                        
-                        // If the output contains data, print it.
-                        if output != "" {
-                            print("Server said: \(output)")
-                        }
-                    }
-                }
-                
-                break;
-            default:
-                print("Received stream event: \(eventCode.rawValue)")
-            }
-        }
     }
 }
