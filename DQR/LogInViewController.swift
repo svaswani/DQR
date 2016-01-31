8b5eb19670c349cbc00307c73e2f68cb1ed43739
Sneha Vaswani <svaswani08@yahoo.com>
2016-01-30 23:15:03 -0500

    Added UI fixes to Login

--- DQR/LogInViewController.swift
+++ DQR/LogInViewController.swift
@@ -10,16 +10,42 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var name: UITextField!
+    
+    @IBOutlet var coolView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "DismissKeyboard")
+        view.addGestureRecognizer(tap)
+        
+        
+        
 
         // Do any additional setup after loading the view.
+    }
+    
+    func DismissKeyboard(){
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+    @IBAction func didStartEditing(sender: AnyObject) {
+        UIView.beginAnimations("lol", context: nil)
+        coolView.transform = CGAffineTransformMakeTranslation(0, -200)
+        UIView.commitAnimations()
+    }
+    @IBAction func hideKeyboard(sender: AnyObject) {
+        UIView.beginAnimations("lol", context: nil)
+        coolView.transform = CGAffineTransformMakeTranslation(0, 0)
+        UIView.commitAnimations()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    
     
 
     /*
